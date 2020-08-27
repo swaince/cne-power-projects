6e9b6fdbe836e5e00d89d05d713e3905714f3fde
zhangth <zhang513044@126.com>
2020-08-27 11:10:17 +0800

    feat(project): init project

---
+++ src/main/java/com/cnegroup/power/CnePowerProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.cnegroup.power;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnePowerProjectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnePowerProjectsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cnegroup/power/CnePowerProjectsApplicationTests.java
@@ -0,0 +1,15 @@
+package com.cnegroup.power;
+
+import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnePowerProjectsApplicationTests {
+
+    @Test
+    @Ignore
+    void contextLoads() {
+    }
+
+}
