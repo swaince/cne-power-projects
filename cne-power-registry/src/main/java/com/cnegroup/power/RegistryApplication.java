3f874827bf66c78c527f25312097d00075b7586d
zhangth <zhang513044@126.com>
2020-08-27 11:21:59 +0800

    feat(project): configure module cne-power-registry

--- cne-power-registry/src/main/java/com/cnegroup/power/RegistryApplication.java
+++ cne-power-registry/src/main/java/com/cnegroup/power/RegistryApplication.java
@@ -2,12 +2,16 @@ package com.cnegroup.power;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author zhangth
+ */
+@EnableEurekaServer
 @SpringBootApplication
-public class CnePowerProjectsApplication {
+public class RegistryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CnePowerProjectsApplication.class, args);
+        SpringApplication.run(RegistryApplication.class, args);
     }
-
 }
