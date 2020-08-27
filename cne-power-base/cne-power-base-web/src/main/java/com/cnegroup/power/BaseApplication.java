fb4620d68568fcbd3ac03bfb939408fc2bf33222
zhangth <zhang513044@126.com>
2020-08-27 13:38:23 +0800

    feat(project): configure module cne-power-base-web

---
+++ cne-power-base/cne-power-base-web/src/main/java/com/cnegroup/power/BaseApplication.java
@@ -0,0 +1,15 @@
+package com.cnegroup.power;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangth
+ */
+@SpringBootApplication
+public class BaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseApplication.class, args);
+    }
+}
